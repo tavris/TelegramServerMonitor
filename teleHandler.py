f11688dec9049b13ac852ae11a5ea68fbdaa8954
tavris <24928728+tavris@users.noreply.github.com>
2021-05-06 17:47:46 +0900

    - Split event handler from main code.

---
+++ teleHandler.py
@@ -0,0 +1,192 @@
+import telegram;
+from telegram.ext import Updater, CommandHandler, CallbackContext;
+
+# TELEGRAM-BOT COMMON ERROR HANDLER #
+def onCommonErrorHandler(update, context):
+	logger.error("Update '%s' cause error '%s'" % update, context);
+# TELEGRAM-BOT COMMON ERROR HANDLER #
+
+# TELEGRAM-BOT COMMAND EVENT HANDLER #
+## HELP COMMAND CALLBACK ##
+def onCommandHelp(update: telegram.Update, _: CallbackContext) -> None:
+	if(not tele.botCmdSecure(update)):	return ;
+
+	helpMsg  = "------------ HelP ------------\n";
+	helpMsg += " * CPU 관련\n";
+	helpMsg += "    - 정보		: /cpu_info\n";
+	helpMsg += "    - 사용량		: /cpu_use\n";
+	helpMsg += " * Memory 관련\n";
+	helpMsg += "    - 사용량		: /mem_use\n";
+	helpMsg += " * Disk 관련\n";
+	helpMsg += "    - 정보		: /disk_info\n";
+	helpMsg += "    - 사용량		: /disk_use\n";
+	helpMsg += " * Network 관련\n";
+	helpMsg += "    - 정보		: /net_info\n";
+	helpMsg += "    - 세부정보	: /net_detail\n";
+	helpMsg += " * Process 관련\n";
+	helpMsg += "    - 정보		: /process_info\n";
+	helpMsg += " * 기타\n";
+	helpMsg += "    - 온도		: /temperature\n";
+	helpMsg += "    - 팬			: /fans\n";
+	helpMsg += "    - 배터리		: /battery\n";
+	helpMsg += "    - 부팅 시간	: /boottime\n";
+	helpMsg += "    - 접속자		: /users\n";
+	helpMsg += "------------------------------\n";
+	update.message.reply_text(helpMsg);
+
+def onCommandCPUInfo(update: telegram.Update, _: CallbackContext) -> None:
+	if(not tele.botCmdSecure(update)):	return ;
+
+	monitor = cMonitor();
+	mInfo = monitor.cpu_info();
+
+	msg  = "* CPU INFO\n";
+	msg += "   - Physical Core : %d\n" % mInfo['pCore'];
+	msg += "   - Logical Core : %d\n" % mInfo['lCore'];
+	msg += "   - CPU Frequency : {} GHz\n".format( round(mInfo['freq'][0]/1024, 1) );
+	update.message.reply_text(msg);
+
+def onCommandCPUUse(update: telegram.Update, _: CallbackContext) -> None:
+	if(not tele.botCmdSecure(update)):	return ;
+
+	monitor = cMonitor();
+	mInfo = monitor.using_cpu_info();
+
+	msg  = "* CPU USE\n";
+	msg += "   - User	: {}%\n".format( mInfo[0] );
+	msg += "   - System	: {}%\n".format( mInfo[2] );
+	msg += "   - idle	: {}%\n".format( mInfo[3] );
+	update.message.reply_text(msg);
+
+def onCommandMemoryInfo(update: telegram.Update, _: CallbackContext) -> None:
+	if(not tele.botCmdSecure(update)):	return ;
+
+	monitor = cMonitor();
+	mInfo = monitor.using_mem_info();
+
+	msg  = "* MEMORY USE\n";
+	msg += "   - [Physical]	Total : %4.1fG, Used : %4.1fG, Free : %4.1fG, Avaliable : %4.1fG\n" % (mInfo['physical'][0]/1024**3, mInfo['physical'][3]/1024**3, mInfo['physical'][4]/1024**3, mInfo['physical'][1]/1024**3);
+	msg += "   - [Swap]		Total : %4.1fG, Used : %4.1fG, Free : %4.1fG\n" % (mInfo['swap'][0]/1024**3, mInfo['swap'][1]/1024**3, mInfo['swap'][2]/1024**3);
+	update.message.reply_text(msg);
+
+def onCommandNetInfo(update: telegram.Update, _: CallbackContext) -> None:
+	if(not tele.botCmdSecure(update)):	return ;
+
+	monitor = cMonitor();
+	mInfo = monitor.net_info();
+
+	msg  = "* NETWORK INFO\n";
+	for k in mInfo['device'].keys():	msg += "   - [%s]	Speed : %d, MTU : %d\n" % (k, mInfo['device'][k][2], mInfo['device'][k][3]);
+
+	if(mInfo['state'][2] > 1000000):	msg += "   - Send : %.0fG packets (%.1fGB)\n" % (mInfo['state'][2]/1000000, mInfo['state'][0]/1024**3);
+	elif(mInfo['state'][2] > 1000):		msg += "   - Send : %.0fK packets (%.1fGB)\n" % (mInfo['state'][2]/1000, mInfo['state'][0]/1024**3);
+	else:	msg += "   - Send : %.0f packets (%.1fGB)\n" % (mInfo['state'][2], mInfo['state'][0]/1024**3);
+
+	if(mInfo['state'][3] > 1000000):	msg += "   - Recv : %.0fG packets (%.1fGB)\n" % (mInfo['state'][3]/1000000, mInfo['state'][1]/1024**3);
+	elif(mInfo['state'][3] > 1000):		msg += "   - Recv : %.0fK packets (%.1fGB)\n" % (mInfo['state'][3]/1000, mInfo['state'][1]/1024**3);
+	else:	msg += "   - Recv : %.0f packets (%.1fGB)\n" % (mInfo['state'][3], mInfo['state'][1]/1024**3);
+	update.message.reply_text(msg);
+
+def onCommandNetDetail(update: telegram.Update, _: CallbackContext) -> None:
+	if(not tele.botCmdSecure(update)):	return ;
+
+	monitor = cMonitor();
+	mInfo = monitor.detail_net_info();
+
+	msg  = "* NETWORK DETAIL\n";
+	msg += "%5s %10s %17s:%5s %10s\n" % ("pid", "name", "IP", "port", "status");
+	for k in mInfo.keys():
+		msg += "%5d %10s %17s:%d %10s" % (k, mInfo[k]['name'], mInfo[k]['raddr'][0], mInfo[k]['raddr'][1], mInfo[k]['status']);
+	update.message.reply_text(msg);
+
+def onCommandTemperature(update: telegram.Update, _: CallbackContext) -> None:
+	if(not tele.botCmdSecure(update)):	return ;
+
+	monitor = cMonitor();
+	mInfo = monitor.temperature_info();
+
+	if(not mInfo):	msg = "This function is not available in the current environment.";
+	else:
+		msg  = "* TEMPERATURE\n";
+
+	update.message.reply_text(msg);
+
+def onCommandFans(update: telegram.Update, _: CallbackContext) -> None:
+	if(not tele.botCmdSecure(update)):	return ;
+
+	monitor = cMonitor();
+	mInfo = monitor.fans_info();
+
+	if(not mInfo):	msg = "This function is not available in the current environment.";
+	else:
+		msg  = "* FAN DETAIL\n";
+
+	update.message.reply_text(msg);
+
+def onCommandBattery(update: telegram.Update, _: CallbackContext) -> None:
+	if(not tele.botCmdSecure(update)):	return ;
+
+	monitor = cMonitor();
+	mInfo = monitor.battery_info();
+
+	if(not mInfo):	msg = "This function is not available in the current environment.";
+	else:
+		msg  = "* BATTERY DETAIL\n";
+
+	update.message.reply_text(msg);
+
+def onCommandBoottime(update: telegram.Update, _: CallbackContext) -> None:
+	if(not tele.botCmdSecure(update)):	return ;
+
+	monitor = cMonitor();
+	mInfo = monitor.boot_time();
+
+	if(not mInfo):	msg = "This function is not available in the current environment.";
+	else:
+		msg  = "* BOOT TIME\n";
+		msg += "   - {}".format(mInfo);
+
+	update.message.reply_text(msg);
+
+def onCommandConnectedUsers(update: telegram.Update, _: CallbackContext) -> None:
+	if(not tele.botCmdSecure(update)):	return ;
+
+	monitor = cMonitor();
+	mInfo = monitor.connect_user();
+
+	if(not mInfo):	msg = "This function is not available in the current environment.";
+	else:
+		msg  = "* CONNECTED USER INFO\n";
+		msg += "%6s %10s %7s %17s\n" % ("PID", "USER", "PTS", "IP");
+		for i in range(len(mInfo)):
+			msg += "%6d %10s %7s %17s\n" % (mInfo[i].pid, mInfo[i].name, mInfo[i].terminal, mInfo[i].host);
+
+	update.message.reply_text(msg);
+
+def onCommandProcess(update: telegram.Update, _: CallbackContext) -> None:
+	if(not tele.botCmdSecure(update)):	return ;
+
+	monitor = cMonitor();
+	mInfo = monitor.process_info();
+
+	msg  = "* PROCESS DETAIL\n";
+	msg += "%5s%5s %25s %10s\n" % ("PID", "PPID", "NAME", "STATUS");
+
+	cnt = 0;
+	for proc in mInfo:
+		msg += "%5d%5d %25s% 10s\n" % (proc.info['pid'], proc.info['ppid'], proc.info['name'], proc.info['status']);
+		cnt += 1;
+		if(cnt >= 30):
+			update.message.reply_text(msg);
+			msg = "%5s%5s  %25s %10s\n" % ("PID", "PPID", "NAME", "STATUS");
+			cnt = 0;
+
+def onCommandMyTelegramID(update: telegram.Update, _: CallbackContext) -> None:
+	msg  = "* USER ID : %s, FID : %s" % (update.message.from_user.name, str(update.message.from_user.id));
+	update.message.reply_text(msg);
+
+def onCommandMyChatroomID(update: telegram.Update, _: CallbackContext) -> None:
+	msg  = "* Chat title : %s, CID : %s" % (update.message.chat.title, str(update.message.chat.id));
+	update.message.reply_text(msg);
+
+# TELEGRAM-BOT COMMAND EVENT HANDLER #
--- teleMonitor.py
+++ teleMonitor.py
@@ -1,9 +1,10 @@
-import os, sys, platform, signal, argparse, logging,  threading, time;
+import os, sys, platform, signal, argparse, logging, threading, time;
 
 import telegram;
 from telegram.ext import Updater, CommandHandler, CallbackContext;
 
 from cMonitor import cMonitor;
+from teleHandler import *;
 
 global tele;
 global logmon;
@@ -35,6 +36,7 @@ class cTelegramBot:
 		try:
 			self.__bot = telegram.Bot(token=key);
 			self.__updater = Updater(token=key);
+			self.__updater.dispatcher.add_error_handler(onCommonErrorHandler);
 			self.__masterList = masters;
 
 			if(cid is not None):	self.__botCid = cid;
@@ -53,201 +55,22 @@ class cTelegramBot:
 		self.__updater.start_polling();
 		self.__updater.idle();
 
+	def getJob(self):
+		return self.__updater.job_queue;
+
 	## Reg new update command ##
-	def setUpdaterEvent(self, event, function, pass_args=False):	self.__updater.dispatcher.add_handler(CommandHandler(event, function, pass_args=pass_args));
+	def setUpdaterEvent(self, event, function, pass_args=False):
+		logger.debug("  - Adding handler for Telegram comment event. (cmd : %s)" % event);
+		self.__updater.dispatcher.add_handler(CommandHandler(event, function, pass_args=pass_args));
 
 	def botSendMessage(self, msg):	self.__bot.sendMessage(chat_id=self.__botCid, text=msg);
 	def botReplyMessage(self, msg):	self.__updater.message.reply_text(msg);
 
 	def botCmdSecure(self, update):
+		logger.debug(update.message.from_user.id in self.__masterList);
 		if(not self.__masterList or update.message.from_user.id in self.__masterList):	return True;
 		else:	return False;
 # TELEGRAM BOT ASSIST CLASS #
-
-# TELEGRAM BOT COMMAND EVENT HANDLER #
-## HELP COMMAND CALLBACK ##
-def onCommandHelp(update: telegram.Update, _: CallbackContext) -> None:
-	if(not tele.botCmdSecure(update)):	return ;
-
-	helpMsg  = "------------ HelP ------------\n";
-	helpMsg += " * CPU 관련\n";
-	helpMsg += "    - 정보		: /cpu_info\n";
-	helpMsg += "    - 사용량		: /cpu_use\n";
-	helpMsg += " * Memory 관련\n";
-	helpMsg += "    - 사용량		: /mem_use\n";
-	helpMsg += " * Disk 관련\n";
-	helpMsg += "    - 정보		: /disk_info\n";
-	helpMsg += "    - 사용량		: /disk_use\n";
-	helpMsg += " * Network 관련\n";
-	helpMsg += "    - 정보		: /net_info\n";
-	helpMsg += "    - 세부정보	: /net_detail\n";
-	helpMsg += " * Process 관련\n";
-	helpMsg += "    - 정보		: /process_info\n";
-	helpMsg += " * 기타\n";
-	helpMsg += "    - 온도		: /temperature\n";
-	helpMsg += "    - 팬			: /fans\n";
-	helpMsg += "    - 배터리		: /battery\n";
-	helpMsg += "    - 부팅 시간	: /boottime\n";
-	helpMsg += "    - 접속자		: /users\n";
-	helpMsg += "------------------------------\n";
-	update.message.reply_text(helpMsg);
-
-def onCommandCPUInfo(update: telegram.Update, _: CallbackContext) -> None:
-	if(not tele.botCmdSecure(update)):	return ;
-
-	monitor = cMonitor();
-	mInfo = monitor.cpu_info();
-
-	msg  = "* CPU INFO\n";
-	msg += "   - Physical Core : %d\n" % mInfo['pCore'];
-	msg += "   - Logical Core : %d\n" % mInfo['lCore'];
-	msg += "   - CPU Frequency : {} GHz\n".format( round(mInfo['freq'][0]/1024, 1) );
-	update.message.reply_text(msg);
-
-def onCommandCPUUse(update: telegram.Update, _: CallbackContext) -> None:
-	if(not tele.botCmdSecure(update)):	return ;
-
-	monitor = cMonitor();
-	mInfo = monitor.using_cpu_info();
-
-	msg  = "* CPU USE\n";
-	msg += "   - User	: {}%\n".format( mInfo[0] );
-	msg += "   - System	: {}%\n".format( mInfo[2] );
-	msg += "   - idle	: {}%\n".format( mInfo[3] );
-	update.message.reply_text(msg);
-
-def onCommandMemoryInfo(update: telegram.Update, _: CallbackContext) -> None:
-	if(not tele.botCmdSecure(update)):	return ;
-
-	monitor = cMonitor();
-	mInfo = monitor.using_mem_info();
-
-	msg  = "* MEMORY USE\n";
-	msg += "   - [Physical]	Total : %4.1fG, Used : %4.1fG, Free : %4.1fG, Avaliable : %4.1fG\n" % (mInfo['physical'][0]/1024**3, mInfo['physical'][3]/1024**3, mInfo['physical'][4]/1024**3, mInfo['physical'][1]/1024**3);
-	msg += "   - [Swap]		Total : %4.1fG, Used : %4.1fG, Free : %4.1fG\n" % (mInfo['swap'][0]/1024**3, mInfo['swap'][1]/1024**3, mInfo['swap'][2]/1024**3);
-	update.message.reply_text(msg);
-
-def onCommandNetInfo(update: telegram.Update, _: CallbackContext) -> None:
-	if(not tele.botCmdSecure(update)):	return ;
-
-	monitor = cMonitor();
-	mInfo = monitor.net_info();
-
-	msg  = "* NETWORK INFO\n";
-	for k in mInfo['device'].keys():	msg += "   - [%s]	Speed : %d, MTU : %d\n" % (k, mInfo['device'][k][2], mInfo['device'][k][3]);
-
-	if(mInfo['state'][2] > 1000000):	msg += "   - Send : %.0fG packets (%.1fGB)\n" % (mInfo['state'][2]/1000000, mInfo['state'][0]/1024**3);
-	elif(mInfo['state'][2] > 1000):		msg += "   - Send : %.0fK packets (%.1fGB)\n" % (mInfo['state'][2]/1000, mInfo['state'][0]/1024**3);
-	else:	msg += "   - Send : %.0f packets (%.1fGB)\n" % (mInfo['state'][2], mInfo['state'][0]/1024**3);
-
-	if(mInfo['state'][3] > 1000000):	msg += "   - Recv : %.0fG packets (%.1fGB)\n" % (mInfo['state'][3]/1000000, mInfo['state'][1]/1024**3);
-	elif(mInfo['state'][3] > 1000):		msg += "   - Recv : %.0fK packets (%.1fGB)\n" % (mInfo['state'][3]/1000, mInfo['state'][1]/1024**3);
-	else:	msg += "   - Recv : %.0f packets (%.1fGB)\n" % (mInfo['state'][3], mInfo['state'][1]/1024**3);
-	update.message.reply_text(msg);
-
-def onCommandNetDetail(update: telegram.Update, _: CallbackContext) -> None:
-	if(not tele.botCmdSecure(update)):	return ;
-
-	monitor = cMonitor();
-	mInfo = monitor.detail_net_info();
-
-	msg  = "* NETWORK DETAIL\n";
-	msg += "%5s %10s %17s:%5s %10s\n" % ("pid", "name", "IP", "port", "status");
-	for k in mInfo.keys():
-		msg += "%5d %10s %17s:%d %10s" % (k, mInfo[k]['name'], mInfo[k]['raddr'][0], mInfo[k]['raddr'][1], mInfo[k]['status']);
-	update.message.reply_text(msg);
-
-def onCommandTemperature(update: telegram.Update, _: CallbackContext) -> None:
-	if(not tele.botCmdSecure(update)):	return ;
-
-	monitor = cMonitor();
-	mInfo = monitor.temperature_info();
-
-	if(not mInfo):	msg = "This function is not available in the current environment.";
-	else:
-		msg  = "* TEMPERATURE\n";
-
-	update.message.reply_text(msg);
-
-def onCommandFans(update: telegram.Update, _: CallbackContext) -> None:
-	if(not tele.botCmdSecure(update)):	return ;
-
-	monitor = cMonitor();
-	mInfo = monitor.fans_info();
-
-	if(not mInfo):	msg = "This function is not available in the current environment.";
-	else:
-		msg  = "* FAN DETAIL\n";
-
-	update.message.reply_text(msg);
-
-def onCommandBattery(update: telegram.Update, _: CallbackContext) -> None:
-	if(not tele.botCmdSecure(update)):	return ;
-
-	monitor = cMonitor();
-	mInfo = monitor.battery_info();
-
-	if(not mInfo):	msg = "This function is not available in the current environment.";
-	else:
-		msg  = "* BATTERY DETAIL\n";
-
-	update.message.reply_text(msg);
-
-def onCommandBoottime(update: telegram.Update, _: CallbackContext) -> None:
-	if(not tele.botCmdSecure(update)):	return ;
-
-	monitor = cMonitor();
-	mInfo = monitor.boot_time();
-
-	if(not mInfo):	msg = "This function is not available in the current environment.";
-	else:
-		msg  = "* BOOT TIME\n";
-		msg += "   - {}".format(mInfo);
-
-	update.message.reply_text(msg);
-
-def onCommandConnectedUsers(update: telegram.Update, _: CallbackContext) -> None:
-	if(not tele.botCmdSecure(update)):	return ;
-
-	monitor = cMonitor();
-	mInfo = monitor.connect_user();
-
-	if(not mInfo):	msg = "This function is not available in the current environment.";
-	else:
-		msg  = "* CONNECTED USER INFO\n";
-		msg += "%6s %10s %7s %17s\n" % ("PID", "USER", "PTS", "IP");
-		for i in range(len(mInfo)):
-			msg += "%6d %10s %7s %17s\n" % (mInfo[i].pid, mInfo[i].name, mInfo[i].terminal, mInfo[i].host);
-
-	update.message.reply_text(msg);
-
-def onCommandProcess(update: telegram.Update, _: CallbackContext) -> None:
-	if(not tele.botCmdSecure(update)):	return ;
-
-	monitor = cMonitor();
-	mInfo = monitor.process_info();
-
-	msg  = "* PROCESS DETAIL\n";
-	msg += "%5s%5s %25s %10s\n" % ("PID", "PPID", "NAME", "STATUS");
-
-	cnt = 0;
-	for proc in mInfo:
-		msg += "%5d%5d %25s% 10s\n" % (proc.info['pid'], proc.info['ppid'], proc.info['name'], proc.info['status']);
-		cnt += 1;
-		if(cnt >= 30):
-			update.message.reply_text(msg);
-			msg = "%5s%5s  %25s %10s\n" % ("PID", "PPID", "NAME", "STATUS");
-			cnt = 0;
-
-def onCommandMyTelegramID(update: telegram.Update, _: CallbackContext) -> None:
-	msg  = "* USER ID : %s, FID : %s" % (update.message.from_user.name, str(update.message.from_user.id));
-	update.message.reply_text(msg);
-
-def onCommandMyChatroomID(update: telegram.Update, _: CallbackContext) -> None:
-	msg  = "* Chat title : %s, CID : %s" % (update.message.chat.title, str(update.message.chat.id));
-	update.message.reply_text(msg);
-
-# TELEGRAM BOT COMMAND EVENT HANDLER #
 
 # TIMMER`S FUNCTION #
 def THRTIME_monitoringCPU():
@@ -257,12 +80,12 @@ def THRTIME_monitoringCPU():
 	logmon.rateCPU.append(monitor.using_cpurate());
 
 	if(logmon.rateCPU[-1] >= args.overheatcpu or abs(logmon.rateCPU[-1]-logmon.rateCPU[-2]) >= args.diffcpu):
+		logger.warning("  Change in CPU usage is {}%".format(logmon.rateCPU[-1] - logmon.rateCPU[-2]));
 		tele.botSendMessage("*CPU usage is {}%.".format(logmon.rateCPU[-1]));
 		pass;
 
 	monThread.append(threading.Timer((args.termonitor * 60), THRTIME_monitoringCPU));
 	monThread[-1].start();
-	logger.info("  - CPU Monitoring Thread Start.");
 
 def THRTIME_monitoringMEM():
 	monitor = cMonitor();
@@ -271,23 +94,30 @@ def THRTIME_monitoringMEM():
 	logmon.rateMEM.append(monitor.using_cpurate());
 
 	if(logmon.rateMEM[-1] >= args.overheatmem or abs(logmon.rateMEM[-1]-logmon.rateMEM[-2]) >= args.diffmem):
+		logger.warning("  Change in MEMORY usage is {}%".format(logmon.rateMEM[-1] - logmon.rateMEM[-2]));
 		tele.botSendMessage("*Memory usage is {}%.".format(logmon.rateMEM[-1]));
 		pass;
 
 	monThread.append(threading.Timer((args.termonitor * 60), THRTIME_monitoringMEM));
 	monThread[-1].start();
-	logger.info("  - Memory Monitoring Thread Start.");
 # TIMMER`S FUNCTION #
 
+def TEST():
+	jb = tele.getJob();
+	jb.start();
+	logger.info(jb.jobs());
+	monThread.append(threading.Timer(10, TEST));
+	monThread[-1].start();
 
 def terminate(signum, frame):
-	for t in monThread:	t.cancel();
+	for t in monThread:
+		if(t.is_alive()):	t.cancel();
 	del logmon;
 	del tele;
 	logger.info("Receive Signal {0}.".format(signum));
 	logger.info("Terminating...");
 	sys.exit(signum);
-
+	pass;
 
 def main():
 	#---------- Global ----------#
@@ -300,17 +130,23 @@ def main():
 
 	tele = cTelegramBot(key=args.teltoken, masters=args.mastertoken, cid=args.chattoken);
 
-	signal.signal(signal.SIGINT, terminate);
-	signal.signal(signal.SIGTERM, terminate);
+	#signal.signal(signal.SIGINT, terminate);
+	#signal.signal(signal.SIGTERM, terminate);
 
 	if(args.termonitor > 0):
 		logmon = cLogMonitor();
 		THRTIME_monitoringCPU();
 		THRTIME_monitoringMEM();
+		TEST();
 		pass;
 
 	## MONITORING COMMNAD ##
-	tele.setUpdaterEvent("help",			onCommandHelp);
+	logger.debug("  * Set event handler.");
+
+	try:
+		tele.setUpdaterEvent("help",			onCommandHelp);
+	except Exception as e:	logger.debug(e);
+	'''
 	tele.setUpdaterEvent("cpu_info",		onCommandCPUInfo);
 	tele.setUpdaterEvent("cpu_use",			onCommandCPUUse);
 	tele.setUpdaterEvent("mem_use",			onCommandMemoryInfo);
@@ -326,12 +162,10 @@ def main():
 
 	tele.setUpdaterEvent("whatismyuserid",	onCommandMyTelegramID);
 	tele.setUpdaterEvent("whatismychatid",	onCommandMyChatroomID);
-
-	logger.info("------ Start Tele ------");
+	'''
 
 	tele.start();
 	## TELEGRAM SETTING ##
-
 	pass;
 
 if __name__=='__main__':
@@ -346,6 +180,9 @@ if __name__=='__main__':
 	parser.add_argument('--cpu_overheat', dest='overheatcpu', type=float, default=85, help="Overheat CPU.");
 	parser.add_argument('--mem_overheat', dest='overheatmem', type=float, default=80, help="Overheat Memory.");
 	parser.add_argument('--term_monitor', dest='termonitor', type=int, default=5, help="Term monitoring. (0 is not monitoring) [Default:5min]");
+	parser.add_argument('--log_path', dest='logpath', type=str, default="/var/log/teleMonitor/", help="Log Path. [Default:/var/log/teleMonitor/]");
+	parser.add_argument('--location_this', dest='locationthis', action='store_false', default=True, help="Develop only");
+
 
 	args = parser.parse_args();
 
@@ -362,9 +199,16 @@ if __name__=='__main__':
 		pass;
 
 	## LOGGER ##
-	logging.basicConfig(level=logging.INFO, format='%(message)s');
 	logger = logging.getLogger("TeleMonitor");
-	logger.addHandler( logging.FileHandler("teleMonitor.logs") );
+	try:
+		if(not os.path.exists(os.path.abspath(args.logpath))):	os.makedirs(os.path.abspath(args.logpath));
+		lHandler = logging.handlers.TimedRotatingFileHandler(filename=os.path.join(os.path.abspath(args.logpath), "teleMonitor.logs"), when='midnight', interval=1, encoding='utf-8');
+		lHandler.suffix = "log-%Y%m%d";
+	except:
+		lHandler = logging.FileHandler("teleMonitor.logs");
+	lHandler.setFormatter(logging.Formatter("[%(process)d] [%(levelname)s] [%(asctime)s]\t%(message)s"));
+	logger.addHandler(lHandler);
+	logger.setLevel(logging.DEBUG);
 	## LOGGER ##
 
 
@@ -381,20 +225,19 @@ if __name__=='__main__':
 
 	## Daemon ##
 	if(args.demon):
-		logger.info("  - Run Daemon mode.");
-
-		import shutil;
-		if(not os.path.exists("/opt/TelegramMonitoring")):
-			os.mkdir("/opt/TelegramMonitoring");
-		if(not os.path.exists("/opt/TelegramMonitoring/teleMonitor.py")):
-			shutil.copy("teleMonitor.py", "/opt/TelegramMonitoring/teleMonitor.py");
-			shutil.copy("cMonitor.py", "/opt/TelegramMonitoring/cMonitor.py");
-			shutil.copy("telemonitor.cfg", "/opt/TelegramMonitoring/telemonitor.cfg");
-		if(not os.path.exists("/etc/systemd/system/teleMonitor.service")):
-			shutil.copy("teleMonitor.service", "/etc/systemd/system/teleMonitor.service");
-			os.system("systemctl daemon-reload");
-			print("Run daemon using 'systemctl start teleMonitor.service' command.");
-			exit(0);
+		if(args.locationthis):
+			import shutil;
+			if(not os.path.exists("/opt/TelegramMonitoring")):
+				os.mkdir("/opt/TelegramMonitoring");
+			if(not os.path.exists("/opt/TelegramMonitoring/teleMonitor.py")):
+				shutil.copy("teleMonitor.py", "/opt/TelegramMonitoring/teleMonitor.py");
+				shutil.copy("cMonitor.py", "/opt/TelegramMonitoring/cMonitor.py");
+				shutil.copy("telemonitor.cfg", "/opt/TelegramMonitoring/telemonitor.cfg");
+			if(not os.path.exists("/etc/systemd/system/teleMonitor.service")):
+				shutil.copy("teleMonitor.service", "/etc/systemd/system/teleMonitor.service");
+				os.system("systemctl daemon-reload");
+				logger.info("Run daemon using 'systemctl start teleMonitor.service' command.");
+				exit(0);
 
 		pid = os.fork();
 		if(pid > 0):	exit(0);
